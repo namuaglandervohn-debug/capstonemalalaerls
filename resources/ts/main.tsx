dc06e492eaa128175554898dc87d9a05a43c69b5
Lander Vohn Namuag <namuag.landervohn@dnsc.edu.ph>
2026-05-15 11:26:42 +0800

    Restore App providers

--- resources/ts/main.tsx
+++ resources/ts/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { RouterProvider } from "react-router";
-import { AuthProvider } from "./app/context/AuthContext";
-import { router } from "./app/routes";
+import App from "./app/App";
 import "./styles/index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
-    <RouterProvider router={router} />
-  </AuthProvider>
-);
+createRoot(document.getElementById("root")!).render(<App />);
